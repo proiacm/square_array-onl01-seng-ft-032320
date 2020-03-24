fd67bb7ddef4f2c88424a3cea3429552e265fbf6
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 21:10:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
- array.each do |num| 
+ array.each do {|num| array << num ** 2}
    #new_array << [num**2]
  end
  
- def new_array(array)
-   array<<[num**2]
-  end
+ #def new_array(array)
+  # array<<[num**2]
+  #end
 end
